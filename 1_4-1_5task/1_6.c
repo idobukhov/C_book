7048e300d64ad08d48a1b1c4359214feb07f96c5
idObukhov <idobukhov06@gmail.com>
2023-08-03 16:03:31 +0500

    Упражнение 1.6-7

---
+++ 1_4-1_5task/1_6.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+main()
+{
+    int c;
+  
+    while (c = getchar() != EOF) {
+        putchar (c);
+        printf ("%d",c);
+    }
+}
+
+
---
+++ 1_4-1_5task/1_7.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+main()
+{
+    int c;
+    c = EOF;
+    printf("%d", c);
+}
