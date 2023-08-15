3edbf74a3c3f64931e4fdf0b6df1d9bb95e64cb6
idObukhov <idobukhov06@gmail.com>
2023-08-15 23:58:22 +0500

    разбор алгоритма 1_9 из книги

---
+++ 1_9task/1_9.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define MAXSYMBLINE 1000
+
+int getline(char line[], int MAXSYMBLINE);
+void copy(char to[], char fromf[]);
+
+int main() {
+    int lenCurrentLine;
+    int lenMaxLine;
+    char currentLine[MAXSYMBLINE];
+    char longest[MAXSYMBLINE];
+    
+    lenMaxLine = 0;
+    while ((lenCurrentLine = getline(currentLine, MAXSYMBLINE)) > 0) 
+        if (lenCurrentLine > lenMaxLine) {
+            lenMaxLine = lenCurrentLine;
+            copy(longest, currentLine);
+        }
+    if (lenMaxLine > 0)
+        printf("%s", longest);
+        
+    return 0;
+}
+
+int getline(char s[], int lim) {
+    int c, i;
+
+    for (i=0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
+        s[i] = c;
+    if (c == 'n') {
+            s[i] = c;
+            ++i;
+    }
+    s[i] = '\0';
+
+    return i;
+}
+
+void copy(char to[], char from[]) {
+    int i = 0;
+    while ((to[i] = from[i]) != '\0')
+        ++i;
+}
+
+
