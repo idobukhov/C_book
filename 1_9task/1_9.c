127952e262a9be88614a5d3f4d00781fcd591c5a
idObukhov <idobukhov06@gmail.com>
2023-08-16 18:57:51 +0500

    change

--- 1_9task/1_9.c
+++ 1_9task/1_9.c
@@ -3,9 +3,9 @@
 #define MAXSYMBLINE 1000
 
 int getline(char line[], int MAXSYMBLINE);
-void copy(char to[], char fromf[]);
+void copy(char to[], char from[]);
 
-int main() {
+int main(void) {
     int lenCurrentLine;
     int lenMaxLine;
     char currentLine[MAXSYMBLINE];
@@ -28,7 +28,7 @@ int getline(char s[], int lim) {
 
     for (i=0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i)
         s[i] = c;
-    if (c == 'n') {
+    if (c == '\n') {
             s[i] = c;
             ++i;
     }
